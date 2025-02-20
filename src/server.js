17b0b4b5019e8f6dd3a7d2779d9d846930942bc9
hudo-ng <hmdong901026@gmail.com>
2025-02-20 16:47:41 -0700

    set up routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import path from "path";
+import router from "./router.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./src/index.html"));
 });
+
+app.use("/api", router);
 
 export default app;
